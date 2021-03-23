0156f2d5c2a188135611658fafb1068d8ce3adc9
Q <Q@DESKTOP-60KPC6M>
2021-03-23 10:43:27 -0400

    add() and iterator() *FUNCTIONAL*

--- 2011assign3/src/A3BSTree.java
+++ 2011assign3/src/A3BSTree.java
@@ -10,7 +10,7 @@ public class A3BSTree<E extends Comparable<? super E>> implements Tree<E> {
 	private class Node {
 		private E item;
 		private Node left, right, parent; // left and right subtrees
-		private int size; // number of nodes in subtree
+		private int size = 0; // number of nodes in subtree
 
 		public Node(E item, int size) {
 			this.item = item;
@@ -24,22 +24,23 @@ public class A3BSTree<E extends Comparable<? super E>> implements Tree<E> {
 
 	@Override
 	public boolean add(E item) {
-
 		root = add(root, item);
 		return true;
 	}
 
 	private Node add(Node x, E item) {
-		if (x == null)
+		if (x == null) {
 			return new Node(item, 1);
+		}
 		int cmp = item.compareTo(x.item);
-		if (cmp < 0)
+		if (cmp < 0) {
 			x.left = add(x.left, item);
-		else if (cmp > 0)
+			x.left.parent = x;}
+		else if (cmp > 0) {
 			x.right = add(x.right, item);
+			x.right.parent = x;}
 		else
 			x.item = item;
-			x.parent = root;
 		x.size = 1 + size(x.left) + size(x.right);
 		return x;
 	}
@@ -65,27 +66,58 @@ public class A3BSTree<E extends Comparable<? super E>> implements Tree<E> {
 	@Override
 	public Iterator<E> iterator() {
 		return new TreeIterator();
-		
+
 	}
-	
-	private class TreeIterator implements Iterator<E>{
+
+	private class TreeIterator implements Iterator<E> {
+		Node next = root;
+
+		public TreeIterator() {
+			if (root == null) {
+				return;
+			}
+
+			while (next.left != null)
+				next = next.left;
+
+		}
 
 		@Override
 		public boolean hasNext() {
 			// TODO Auto-generated method stub
-			return false;
+			return next != null;
 		}
 
 		@Override
 		public E next() {
-			// TODO Auto-generated method stub
-			return null;
+
+			Node r = next;
+
+			// If you can walk right, walk right, then fully left.
+			// otherwise, walk up until you come from left.
+			if (next.right != null) {
+				next = next.right;
+				while (next.left != null)
+					next = next.left;
+				return r.item;
+			}
+
+			while (true) {
+				if (next.parent == null) {
+					next = null;
+//					System.out.println("hi");
+					return r.item;
+				}
+				if (next.parent.left == next) {
+//					System.out.println("hui");
+					next = next.parent;
+					return r.item;
+				}
+				next = next.parent;
+			}
+
 		}
-	  
-	 }
-	
-	
-	
+	}
 
 	@Override
 	public int height() {
@@ -106,16 +138,26 @@ public class A3BSTree<E extends Comparable<? super E>> implements Tree<E> {
 			return x.size;
 	}
 
-	/*for testing DELETE BEFORE SUBMITTING*/
+	/* for testing DELETE BEFORE SUBMITTING */
 	public static void main(String[] args) {
-		Tree <Integer> tree = new A3BSTree<>();
+		Tree<Integer> tree = new A3BSTree<>();
 
-		for (Integer i = 0; i < 10; i++) { 
-		System.out.println(i);
-		tree.add(i);
+		tree.add(3);
+		tree.add(5);
+		tree.add(1);
+		tree.add(4);
+		tree.add(2);
+		tree.add(7);
+		tree.add(23);
+		tree.add(0);
+		tree.add(69);
+
+		Iterator<Integer> iterator = tree.iterator();
+		while (iterator.hasNext()) {
+			Integer item = iterator.next();
+			System.out.println(item);
 		}
-		
-		System.out.println();
+
 	}
 
 }

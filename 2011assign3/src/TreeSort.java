3efffeec9ff4dbba158232ca68e49815f61680ac
Q <Q@DESKTOP-60KPC6M>
2021-03-28 16:45:37 -0400

    Treesort changes

--- 2011assign3/src/TreeSort.java
+++ 2011assign3/src/TreeSort.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class TreeSort{
 	/** Sorts an array using TreeSort with a balanced BST implementation 
 	 * @param a an array to sort
@@ -13,6 +15,15 @@ public class TreeSort{
 	 * @param a an array to sort
 	 */
 	public static <E extends Comparable <? super E>> void sort(Tree <E> tree, E[] a){
-		// TODO Auto-generated method stub
+		Iterator<E> iterator;
+        for (int i = 0; i < a.length; i++) {
+            tree.add(a[i]);
+        }
+        iterator = tree.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            a[i] = iterator.next();
+            i++;
+        }
 	}
 }
